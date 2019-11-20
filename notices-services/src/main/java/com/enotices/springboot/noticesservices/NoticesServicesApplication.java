ff9e168284734b8547f0bb1263eecb8a3cbeb8fb
jgerlan <jgerlan@gmail.com>
2019-11-20 15:13:43 -0300

    [#0001] Start Project

---
+++ notices-services/src/main/java/com/enotices/springboot/noticesservices/NoticesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.enotices.springboot.noticesservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticesServicesApplication.class, args);
+	}
+
+}
---
+++ notices-services/src/test/java/com/enotices/springboot/noticesservices/NoticesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enotices.springboot.noticesservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticesServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
