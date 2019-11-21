7ed3e10305d14c21a0793c0377808b69af6892f9
Gerlan de Paula <jgerlan@gmail.com>
2019-11-21 09:42:49 -0300

    [#0008] List a notice.

--- notices-services/src/main/java/com/enotices/springboot/noticesservices/repository/NewsRepository.java
+++ notices-services/src/main/java/com/enotices/springboot/noticesservices/repository/NewsRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.enotices.springboot.noticesservices.models.News;
 
 public interface NewsRepository extends JpaRepository<News, Long>{
+	News findById(long id);
 	
 }
